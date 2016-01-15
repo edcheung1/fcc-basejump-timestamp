8df20e6c520b1cba3dfdf8dc82930482d1422516
Ed <echeung1991@gmail.com>
2016-01-15 12:31:03 -0600

    finished timestamp service

---
+++ app/routes/index.js
@@ -0,0 +1,48 @@
+'use strict'
+
+module.exports = function(app) {
+	app.route('/')
+		.get(function (req, res) {
+			res.sendFile(process.cwd() + '/public/index.html');
+		});
+	app.route('/*')
+		.get(function (req, res) {
+			var isNum = /^\d+$/;
+			var unixOut;
+			
+			var apiPath = decodeURI(req.path.substring(1));
+			
+			if(isNum.test(apiPath)) {
+				var parseDate = new Date(apiPath*1000);
+				unixOut = apiPath;
+			} else if (!isNaN(Date.parse(apiPath))) {
+				var parseDate = new Date(apiPath);
+				unixOut = Date.parse(apiPath);
+			} else {
+				res.json({unix: null, natural: null});
+				return;
+			}
+			
+			var year = parseDate.getFullYear();
+			var month = natMonth[parseDate.getMonth()];
+			var date = parseDate.getDate();
+			var natDate = month +' '+ date +', '+ year;
+			
+			res.json({unix: unixOut, natural: natDate});
+		});
+};
+
+var natMonth = {
+	'0': 'January',
+	'1': 'Feburary',
+	'2': 'March',
+	'3': 'April',
+	'4': 'May',
+	'5': 'June',
+	'6': 'July',
+	'7': 'August',
+	'8': 'September',
+	'9': 'October',
+	'10': 'November',
+	'11': 'December'
+};
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var express = require('express');
+'use strict';
+
+var express = require('express'),
+	routes = require('./app/routes/index.js');
 var app = express();
+var router = express.Router();
+
+/* app.use(router); */
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-	response.send("blah");
-});
+routes(app);
+
+/* router.get('/', function(request, response) {
+	response.sendfile(process.cwd() + '/index.html');
+}); */
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port ', app.get('port'));
 });
